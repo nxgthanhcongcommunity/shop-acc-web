db9489ac12162a22448872910f73fb8616cd5015
nxgthanhcong <73934789+nxgthanhcong@users.noreply.github.com>
2024-04-23 07:29:13 +0700

    ..

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
-export { default as MainSlider } from "./main-slider";
-export { default as SubSlider } from "./sub-slider";
-export { default as Section } from "./section";
 export { default as Button } from "./button";
-export { default as MobileNavbar } from "./mobile-navbar";
+export { default as MainSlider } from "./main-slider";
 export { default as MobileMenuItem } from "./mobile-menu-item";
+export { default as MobileNavbar } from "./mobile-navbar";
+export { default as Section } from "./section";
+export { default as SubSlider } from "./sub-slider";
 //MobileMenuItem
--- src/components/mobile-navbar/index.tsx
+++ src/components/mobile-navbar/index.tsx
@@ -1,13 +1,84 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../../stores";
+import { IMenuItemProps } from "../../types";
+import MenuItem from "../menu-item";
+import { MobileMenuItem } from "..";
 
 const MobileNavbar = (props: any) => {
+  const { activedMobileNav } = useSelector((state: RootState) => state.app);
 
-  const {activedMobileNav} = useSelector((state: RootState) => state.app)
+  const menuItems: IMenuItemProps[] = [
+    {
+      title: "store",
+      subTitle: "games",
+      subMenuItems: [
+        {
+          title: "Store Style 1",
+          href: "/",
+        },
+        {
+          title: "Store Style 1",
+          href: "/",
+        },
+        {
+          title: "Store Style 1",
+          href: "/",
+        },
+      ],
+    },
+    {
+      title: "blog",
+      subTitle: "news",
+      subMenuItems: [
+        {
+          title: "Store Style 1",
+          href: "/",
+        },
+        {
+          title: "Store Style 1",
+          href: "/",
+        },
+        {
+          title: "Store Style 1",
+          href: "/",
+        },
+      ],
+    },
+    {
+      title: "features",
+      subTitle: "full",
+      subMenuItems: [
+        {
+          title: "Store Style 1",
+          href: "/",
+        },
+        {
+          title: "Store Style 1",
+          href: "/",
+        },
+        {
+          title: "Store Style 1",
+          href: "/",
+        },
+      ],
+    },
+  ];
 
   return (
     <div>
-      <div className={`fixed duration-300 top-0 h-[100vh] w-[60vw] lg:hidden bg-[rgba(8,3,37,1)] z-50 ${activedMobileNav ? "right-0" : "-right-full"}`}></div>
+      <div
+        className={`fixed top-0 z-50 h-[100vh] w-[60vw] bg-[rgba(8,3,37,1)] duration-300 lg:hidden ${activedMobileNav ? "right-0" : "-right-full"}`}
+      >
+        <div className="absolute left-8 top-[15%]">
+          <ul className="grid gap-y-4">
+            {menuItems.map((item) => (
+              <li>
+                <MobileMenuItem {...item} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
